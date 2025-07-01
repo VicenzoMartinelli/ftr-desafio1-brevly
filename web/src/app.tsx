0bdecf52d814eb5cdf3de023d62b1264c3db8f51
Vicenzo Martinelli <martinellivicenzo@gmail.com>
2025-06-30 22:03:22 -0300

    add web project

---
+++ web/src/app.tsx
@@ -0,0 +1,10 @@
+export function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <button className="bg-white border-primary border-2 text-white text-md font-bold py-2 px-4 rounded hover:bg-primary hover:text-white">
+        Teste
+      </button>
+    </>
+  )
+}
